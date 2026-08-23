df5b40f7b5dead0e7cda95d5f2649f6fb0b37561
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 14:51:20 +0330

    committed by script

--- site/parts/purchaseOrderSummary.jsx
+++ site/parts/purchaseOrderSummary.jsx
@@ -1,4 +1,4 @@
-export default ({ purchaseOrder }) => <dl class='purchaseOrderSummary'>
+export default ({ purchaseOrder }) => <dl class='summary'>
     <dt class='number'>{purchaseOrder.number}</dt>
     <dd class='status'>{purchaseOrder.purchaseOrderStatus}</dd>
     <dd class='total'>{purchaseOrder.total}</dd>
